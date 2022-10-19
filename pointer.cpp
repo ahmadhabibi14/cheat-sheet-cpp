56f38f5d990b9746f872ec228b12a65105a83027
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2022-10-20 06:03:51 +0800

    Kode

--- pointer.cpp
+++ pointer.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 #include <string>
+
+using namespace std;
+
+int main() {
+    int var = 20;
+    int *ptr;
+    ptr = &var;
+
+    cout << var << endl;
+
+    cout << ptr << endl;
+
+    cout << *ptr << endl;
+    return 0;
+}
